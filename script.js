a74fa85c509f1a4b6d676f93d67777b254141f52
Wilson Rocha <132412189+wijaro88@users.noreply.github.com>
2023-06-10 08:47:03 -0500

    1er commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// alert("r");
+//EJERCICIO 1
+// alert("imprime numeros impares")
+// let num;
+
+// for (let i = 1; i <= 50; i++) {
+//     if(i%2 != 0){
+//         console.log(i);
+//     }   
+//     }
+
+//EJERCICIO 2
+
+// let pokemons = 
+// [ 'bulbasaur',
+//   'ivysaur',
+//   'venusaur',
+//   'charmander',
+//   'charmeleon',
+//   'charizard',
+//   'squirtle',
+//   'wartortle',
+//   'blastoise',
+//   'caterpie',
+//   'metapod',
+//   'butterfree',
+//   'weedle',
+//   'kakuna',
+//   'beedrill',
+//   'pidgey',
+//   'pidgeotto',
+//   'pidgeot',
+//   'rattata',
+//   'raticate',
+//   'spearow',
+//   'fearow',
+//   'ekans',
+//   'arbok',
+//   'pikachu',
+//   'raichu',
+//   'sandshrew',
+//   'sandslash',
+//   'nidoran-f',
+//   'nidorina',
+//   'nidoqueen',
+//   'nidoran-m',
+//   'nidorino',
+//   'nidoking',
+//   'clefairy',
+//   'clefable',
+//   'vulpix',
+//   'ninetales',
+//   'jigglypuff',
+//   'wigglytuff',
+//   'zubat',
+//   'golbat',
+//   'oddish',
+//   'gloom',
+//   'vileplume',
+//   'paras',
+//   'parasect',
+//   'venonat',
+//   'venomoth',
+//   'diglett',
+//   'dugtrio',
+//   'meowth',
+//   'persian',
+//   'psyduck',
+//   'golduck',
+//   'mankey',
+//   'primeape',
+//   'growlithe',
+//   'arcanine',
+//   'poliwag',
+//   'poliwhirl',
+//   'poliwrath',
+//   'abra',
+//   'kadabra',
+//   'alakazam',
+//   'machop',
+//   'machoke',
+//   'machamp',
+//   'bellsprout',
+//   'weepinbell',
+//   'victreebel',
+//   'tentacool',
+//   'tentacruel',
+//   'geodude',
+//   'graveler',
+//   'golem',
+//   'ponyta',
+//   'rapidash',
+//   'slowpoke',
+//   'slowbro',
+//   'magnemite',
+//   'magneton',
+//   'farfetchd',
+//   'doduo',
+//   'dodrio',
+//   'seel',
+//   'dewgong',
+//   'grimer',
+//   'muk',
+//   'shellder',
+//   'cloyster',
+//   'gastly',
+//   'haunter',
+//   'gengar',
+//   'onix',
+//   'drowzee',
+//   'hypno',
+//   'krabby',
+//   'kingler',
+//   'voltorb' ]
+//   let num = parseInt(prompt("digite el numero"));
+//   for (let i = 0; i <= num; i++) {
+//     let j=i+1;
+//     if (j%5 == 0 ) {
+//         console.log(pokemons[i]);
+//     }  
+// }
+
+//EJERCICIO 3
+
+// let arre = [4,"dos",8,"tres",5,9,1,"cero"];
+// for (let i = 0; i < arre.length; i++) {
+//     if(isNaN(arre[i])==false ){
+//     console.log(arre[i]);
+//     }
+// }
+
